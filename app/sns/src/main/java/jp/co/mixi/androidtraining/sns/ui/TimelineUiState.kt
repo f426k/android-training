aaedb65ff5cb9627e58ad91dcc90d31b4f5b59dd
fuka-takashima <fuka.takashima@dena.com>
2024-02-21 19:55:09 +0900

    snsのリファクタリング演習

---
+++ app/sns/src/main/java/jp/co/mixi/androidtraining/sns/ui/TimelineUiState.kt
@@ -0,0 +1,9 @@
+package jp.co.mixi.androidtraining.sns.ui
+
+import jp.co.mixi.androidtraining.sns.data.entity.Post
+
+sealed class TimelineUiState{
+    data class Success(val posts: List<Post> = emptyList()): TimelineUiState()
+    object  Loading: TimelineUiState()
+    object  Error: TimelineUiState()
+}
---
+++ app/sns/src/main/java/jp/co/mixi/androidtraining/sns/ui/TimelineViewModel.kt
@@ -0,0 +1,31 @@
+package jp.co.mixi.androidtraining.sns.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import jp.co.mixi.androidtraining.sns.data.repository.DefaultTimelineRepository
+import jp.co.mixi.androidtraining.sns.data.repository.TimelineRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+class TimelineViewModel(
+    private val repository: TimelineRepository = DefaultTimelineRepository()
+): ViewModel() {
+
+    var uiState by mutableStateOf<TimelineUiState>(TimelineUiState.Success())
+        private set
+
+    fun getTimeline() {
+        viewModelScope.launch {
+            uiState = TimelineUiState.Loading
+            uiState = try {
+                val posts = repository.getTimeline()
+                TimelineUiState.Success(posts = posts)
+            } catch (error: IOException) {
+                TimelineUiState.Error
+            }
+        }
+    }
+}
